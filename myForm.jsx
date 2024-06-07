5db636da8cfed3ccba5064720ef714bc09db1e83
valentinaaattardo <161291505+valentinaaattardo@users.noreply.github.com>
2024-06-07 17:37:48 +0200

    CONTROLLED FORMS -5

--- myForm.jsx
+++ myForm.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
-
-function createData() {
-    return {
-        username: '',
-        password: '',
-        session: false, 
-    }
-}
-
-export function MyForm() {
+export function myForm() {
     const [data, setData] = useState(createData)
-
-
+       
     function handlerInputChange(event) {
         const name = event.target.name;
         const value = event.target.value;
         const checked = event.target.checked;
         const type = event.target.type;
-
         setData(prevData => {
             const updated = {
                 ...prevData,
@@ -26,9 +14,12 @@ export function MyForm() {
             return updated;
         });
     }
-
     function handleResetForm() {
         setData(createData);
+    }
+
+    function handleLogin(event) {
+        event.preventDefault()
     }
 
     return (
@@ -38,7 +29,7 @@ export function MyForm() {
             <input name="password" type="password" value={data.password} onChange={handlerInputChange} />
             <input name="session" type="checkbox" checked={data.session} onChange={handlerInputChange} />
             <button onClick={handleResetForm}>Reset</button>
-            <button disabled={!data.username || !data.password}>Login</button>
+            <button disabled={!data.username || !data.password} onSubmit={handleLogin}>Login</button>
             <pre>
                 {JSON.stringify(data, null, 2)}
             </pre>
